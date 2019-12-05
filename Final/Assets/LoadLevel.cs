3bd8d840868f9fe859e30d8d07a75807dabf3a62
kydo5322 <kydo5322@colorado.edu>
2019-12-05 10:51:42 -0700

    level change

---
+++ Final/Assets/LoadLevel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    public string levelName;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //load level with scene name
+            SceneManager.LoadScene(levelName);
+
+            //restart level 1
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
