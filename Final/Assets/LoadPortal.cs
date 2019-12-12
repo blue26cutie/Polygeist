349fc763a41b526592a84de45d391adc9ae302a1
Claire Ink <45399318+blue26cutie@users.noreply.github.com>
2019-12-12 11:08:50 -0700

    LoadPortal Script

---
+++ Final/Assets/LoadPortal.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadPortal : MonoBehaviour
+{
+    public int scoreVal = 1;
+    public GameObject portal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        portal.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ScoreManager.score == scoreVal)
+        {
+            spawnPortal();
+        }
+    }
+
+    void spawnPortal()
+    {
+
+        portal.SetActive(true);
+        Debug.Log("portal active");
+    }
+}
